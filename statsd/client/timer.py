b0eaec4ff4a3ae60fa9db7939ce40449bac99b94
Matthew Hughes <matthewhughes934@gmail.com>
2022-07-17 17:10:27 +0100

    Remove unnecessary import guard
    We don't need this since 5763d98e1c062c58a1fbf82f81c96c13e8326ece

--- statsd/client/timer.py
+++ statsd/client/timer.py
@@ -1,12 +1,5 @@
 import functools
-
-# Use timer that's not susceptible to time of day adjustments.
-try:
-    # perf_counter is only present on Py3.3+
-    from time import perf_counter as time_now
-except ImportError:
-    # fall back to using time
-    from time import time as time_now
+from time import perf_counter as time_now
 
 
 def safe_wraps(wrapper, *args, **kwargs):
